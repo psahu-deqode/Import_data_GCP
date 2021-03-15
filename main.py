29f3e3974c13a94beb8fa8033f25ccbfea442154
Puja Sahu <psahu@deqode.com>
2021-03-15 21:38:14 +0530

    changes

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import json
 import os
+import json
 from google.cloud import storage, datastore
-from dotenv import load_dotenv
-load_dotenv()
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+SERVICE_ACCOUNT_JSON = os.getenv('SERVICE_ACCOUNT_JSON')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 
 
 def main():
     # create storage client
-    storage_client = storage.Client.from_service_account_json(os.getenv('service_account_json'))
+    storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
     # get bucket with name
-    bucket = storage_client.get_bucket(os.getenv('bucket_name'))
-    project_id = os.getenv('project_id')
+    bucket = storage_client.get_bucket(BUCKET_NAME)
     # get bucket data as blob
-    blob = bucket.get_blob('doc1.json')
+    blob = bucket.get_blob('contentsDB_32TYFF02_1.00.00_en_GB_Navi_20200408.json')
     data = json.loads(blob.download_as_string())
     # create datastore entity
-    imported_json = datastore.Entity(key=datastore.Client(project_id).key("data"))
+    imported_json = datastore.Entity(key=datastore.Client(PROJECT_ID).key("data"))
     # update datastore entity with the imported json
     imported_json.update(data)
-    datastore.Client(project_id).put(imported_json)
+    datastore.Client(PROJECT_ID).put(imported_json)
 
 
 if __name__ == "__main__":
