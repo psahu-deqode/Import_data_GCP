b7f4fd8a50d335b7884f631a10b775565ea532c3
Puja Sahu <psahu@deqode.com>
2021-03-15 20:17:09 +0530

    updated code and requirements.txt

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import json
+import os
+from google.cloud import storage, datastore
+from dotenv import load_dotenv
+load_dotenv()
 
-from google.cloud import storage,datastore
 
-
-def import_from_gcs_to_datastore():
+def main():
     # create storage client
-    storage_client = storage.Client.from_service_account_json('/home/puja/Downloads/tripbot-cloud-dda1f4e5fdab.json')
+    storage_client = storage.Client.from_service_account_json(os.getenv('service_account_json'))
     # get bucket with name
-    bucket = storage_client.get_bucket('data-import-gcs')
-    project_id = 'tripbot-cloud'
+    bucket = storage_client.get_bucket(os.getenv('bucket_name'))
+    project_id = os.getenv('project_id')
     # get bucket data as blob
     blob = bucket.get_blob('doc1.json')
-
-    # convert to string
-    json_data_string = blob.download_as_string().decode('utf-8').replace("'", '"')
-    data = json.loads(json_data_string)
+    data = json.loads(blob.download_as_string())
     # create datastore entity
     imported_json = datastore.Entity(key=datastore.Client(project_id).key("data"))
     # update datastore entity with the imported json
@@ -22,4 +21,5 @@ def import_from_gcs_to_datastore():
     datastore.Client(project_id).put(imported_json)
 
 
-import_from_gcs_to_datastore()
+if __name__ == "__main__":
+    main()
